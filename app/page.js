a264af840a1d03fcd81a076c876af1f3199ffaa7
enjiwood <87221016+enjiwood@users.noreply.github.com>
2024-08-08 18:49:33 -0700

    Completed pantry tracker

--- app/page.js
+++ app/page.js
@@ -12,6 +12,7 @@ export default function Home() {
   const [inventory, setInventory] = useState([])
   const [open, setOpen] = useState(false)
   const [itemName, setItemName] = useState('')
+  const [itemSearch, setItemSearch] = useState('')
 
   const updateInventory = async () => {
     const snapshot = query(collection(firestore, 'inventory'))
@@ -55,6 +56,26 @@ export default function Home() {
     await updateInventory()
   }
 
+  const customSearch = async (item) => {
+    if (item === ""){
+      await updateInventory()
+      return
+    }
+    const snapshot = query(collection(firestore, 'inventory'))
+    const docs = await getDocs(snapshot)
+    const searchList = []
+    docs.forEach((doc) => {
+      if (doc.id.toLowerCase().includes(item.toLowerCase())){
+        searchList.push({
+          name: doc.id,
+          ...doc.data(),
+        })
+      }
+    })
+
+    setInventory(searchList)
+  }
+
   useEffect(() => {
     updateInventory()
   }, [])
@@ -64,14 +85,15 @@ export default function Home() {
 
   return (
     <Box width="100vw" height="100vh" display="flex" flexDirection="column" justifyContent = "center" alignItems="center" gap={2}>
-      <Modal open={open} close={handleClose}>
+      <Modal open={open} close={handleClose} onClose={handleClose}>
         <Box
         position="absolute"
         top="50%" left="50%"
         width={400}
-        bgcolor ="white"
-        border="2px solid #FFF"
-        boxShadow={24}
+        bgcolor ="#1F1F1F"
+        border="2px solid #6874E8"
+        borderRadius="15px"
+        boxShadow="0px 0px 20px #6874E8"
         p={4}
         display="flex"
         flexDirection="column"
@@ -79,32 +101,61 @@ export default function Home() {
         sx = {{
           transform: "translate(-50%, -50%)",
         }}>
-          <Typography variant="h6">Add Item</Typography>
+          <Stack width = "100%" direction="row" spacing = {2}>
+            <Typography variant="h6" color="#CCDDE2">Add Item</Typography>
+          </Stack>
+          
           <Stack width="100%" direction = "row" spacing ={2}>
             <TextField
-            variant = "filled"
-            color = "secondary"
+            variant = "outlined"
             fullWidth
+            autoFocus
             value = {itemName}
             onChange = {(e) =>{
               setItemName(e.target.value)
             }}
             sx = {{
-              border:"2px solid #FFF",
+              border:"2px solid #FFF", borderRadius:"5px", input: { color: "#E7E6F7" },
+            }}
+            onKeyDown={(ev) => {
+              console.log(`Pressed keyCode ${ev.key}`);
+              if (ev.key === 'Enter') {
+                addItem(itemName)
+                setItemName('')
+                handleClose()
+                ev.preventDefault()
+              }
             }}
             />
-            <Button variant="outlined" onClick = {() => {
+            <Button variant="text" onClick = {() => {
               addItem(itemName)
               setItemName('')
               handleClose()
-            }}>Add</Button>
+            }}><Typography variant="h6" color="green">Add</Typography></Button>
           </Stack>
         </Box>
       </Modal>
-      <Button variant = "text" onClick = {() => {
-        handleOpen()
-      }}
-      ><Typography color="#E7E6F7">Add New Item</Typography></Button>
+
+      <Box width="800px" height = "50px" justifyContent="space-between" display="flex" alignItems="center">
+        <Button variant = "text" onClick = {() => {
+          handleOpen()
+        }}
+        ><Typography color="#E7E6F7">Add New Item</Typography></Button>
+
+        <TextField id = "outlined-helperText" fullwidth color = "secondary" size = "small" label = "Search" type = "search" value = {itemSearch} onChange = {(e) => {
+          setItemSearch(e.target.value)
+        }} sx = {{border: "2px transparent #000", borderRadius:"5px", input: { color: "#E7E6F7" },}} 
+        onKeyDown={(ev) => {
+          console.log(`Pressed keyCode ${ev.key}`);
+          if (ev.key === 'Enter') {
+            customSearch(itemSearch)
+            ev.preventDefault()
+          }
+        }}/>
+      </Box>
+      
+
+
       <Box border = "1px solid #333" borderRadius="15px" boxShadow="0px 0px 200px #6874E8">
         <Box width="800px" height="100px" bgcolor="#6874E8" borderRadius="15px 15px 0px 0px" 
         alignItems = "center" justifyContent = "center" display = "flex">
